8afac0243722bad286c97b14666bbb65884f241d
Prakash <prakash2756>
2025-08-14 16:50:54 +0530

    Transaction model added

--- Bank/models.py
+++ Bank/models.py
@@ -1,5 +1,7 @@
 import random
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
@@ -57,3 +59,21 @@ class IoBaccount(models.Model):
         def __str__(self):
             return f"{self.account_name}{self.bank_name}{self.dob}{self.account_request}{self.email_address}"
 
+class Transaction(models.Model):    
+    CREDIT = 'CR'
+    DEBIT = 'DR'
+    TYPE_CHOICES = [
+        (CREDIT, 'Credit'),
+        (DEBIT, 'Debit'),
+    ]
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()    
+    user = GenericForeignKey('content_type','object_id')
+    
+    trans_type = models.CharField(max_length=20,choices=TYPE_CHOICES)
+    description = models.CharField(max_length=200,blank=True,null=True)
+    tran_date = models.DateTimeField(auto_now_add=True)
+    receiver_name = models.CharField(max_length=200,blank=True,null=True)
+    receiver_amount = models.CharField(max_length=200,blank=True,null=True)
+    
+    
